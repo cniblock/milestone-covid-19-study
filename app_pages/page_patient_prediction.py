a3045124b0a59587f8a63d1f0977c1ccafca79e5
Chris Niblock <nibbo1984@hotmail.co.uk>
2024-08-26 21:45:42 +0000

    added feat scaling to model pipeline

--- app_pages/page_patient_prediction.py
+++ app_pages/page_patient_prediction.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import joblib
-import matplotlib.pyplot as plt
 
 def load_pipeline(base_path):
     return joblib.load(base_path)
@@ -12,12 +11,13 @@ def page_patient_prediction_body():
     model_pipeline_reg = load_pipeline(model_path_reg)
 
     input_data = get_user_input()
+    st.write("Input Data:", input_data)
 
     if st.button("Run Predictive Analysis"):
         try:
             reg_prediction = model_pipeline_reg.predict(input_data)[0]
 
-            if reg_prediction < 0.5:
+            if reg_prediction < 0.68:
                 risk_level_reg = "Low Risk"
                 risk_color_reg = "green"
             elif reg_prediction < 0.8:
@@ -38,28 +38,35 @@ def get_user_input():
 
     age = st.number_input("Age", min_value=0, max_value=120, value=20)
     intubed = st.selectbox("Intubed", ["Yes", "No"])
-    icu = st.selectbox("ICU", ["Yes", "No"])
-    pneumonia = st.selectbox("Pneumonia", ["Yes", "No"])
     hypertension = st.selectbox("Hypertension", ["Yes", "No"])
     diabetes = st.selectbox("Diabetes", ["Yes", "No"])
     obesity = st.selectbox("Obesity", ["Yes", "No"])
+    other_disease = st.selectbox("Other Disease", ["Yes", "No"])
+    icu = st.selectbox("ICU", ["Yes", "No"])
+    pneumonia = st.selectbox("Pneumonia", ["Yes", "No"])
+    cardiovascular = st.selectbox("Cardiovascular", ["Yes", "No"])
+    inmsupr = st.selectbox("Immunosuppressed", ["Yes", "No"])
+    renal_chronic = st.selectbox("Renal Chronic", ["Yes", "No"])
+    tobacco = st.selectbox("Tobacco", ["Yes", "No"])
+    copd = st.selectbox("COPD", ["Yes", "No"])
+    asthma = st.selectbox("Asthma", ["Yes", "No"])
 
     input_data = pd.DataFrame({
-        'AGE': [age],
-        'INTUBED': [1 if intubed == "Yes" else 0],
-        'ICU': [1 if icu == "Yes" else 0],
-        'PNEUMONIA': [1 if pneumonia == "Yes" else 0],
-        'HIPERTENSION': [1 if hypertension == "Yes" else 0],
-        'DIABETES': [1 if diabetes == "Yes" else 0],
-        'OBESITY': [1 if obesity == "Yes" else 0],
-        'SEX': [0],
-        'COPD': [0],
-        'ASTHMA': [0],
-        'INMSUPR': [0],
-        'OTHER_DISEASE': [0],
-        'CARDIOVASCULAR': [0],
-        'RENAL_CHRONIC': [0],
-        'TOBACCO': [0]
-    })
+    'AGE': [age],
+    'INTUBED': [0 if intubed == "Yes" else 1],
+    'HIPERTENSION': [0 if hypertension == "Yes" else 1],
+    'DIABETES': [0 if diabetes == "Yes" else 1],
+    'OBESITY': [0 if obesity == "Yes" else 1],
+    'OTHER_DISEASE': [0 if other_disease == "Yes" else 1],
+    'ICU': [0 if icu == "Yes" else 1],
+    'PNEUMONIA': [0 if pneumonia == "Yes" else 1],
+    'CARDIOVASCULAR': [0 if cardiovascular == "Yes" else 1],
+    'INMSUPR': [0 if inmsupr == "Yes" else 1],
+    'RENAL_CHRONIC': [0 if renal_chronic == "Yes" else 1],
+    'TOBACCO': [0 if tobacco == "Yes" else 1],
+    'COPD': [0 if copd == "Yes" else 1],
+    'ASTHMA': [0 if asthma == "Yes" else 1],
+    'SEX': [0]
+})
 
     return input_data
